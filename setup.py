95539fe1afc15c3870a4c4588042ba94af93d97d
Famz21 <famtomoe250@gmail.com>
2025-07-05 15:24:42 +0700

    updated

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="MLOPS-PROJECT-2",
+    name="Recommendation-System",
     version="0.1",
-    author="Sudhanshu",
+    author="VIRA",
     packages=find_packages(),
     install_requires = requirements,
 )
